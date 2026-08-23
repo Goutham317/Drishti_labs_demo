5e5727d0bb07589d99a87484f47c35ec2a8bc934
Goutham317 <gkkrishna317@gmail.com>
2026-08-23 12:13:04 +0530

    logo changed

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
+// 1. Import your new logo file at the very top (make sure the filename matches yours!)
+import myLogo from '../assets/drishtilogo.png'; 
+
 export default function Navbar() {
   return (
     <nav className="nav">
       <div className="container nav-inner">
         <a href="#top" className="brand">
-          <span className="brand-mark"></span>
+          
+          {/* 2. Replace the 'brand-mark' span with your image tag */}
+          <img src={myLogo} alt="Drishti Labs Logo" className="logo-img" style={{ height: '40px', width: 'auto' }} />
+          
+          {/* Keep or remove this text depending on if your image already has text in it */}
           <span>DRISHTI LABS</span>
+          
         </a>
         <div className="nav-links">
           <a href="#problem">Problem</a>
